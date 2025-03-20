dbbeada8f5f8edb35dd8edd343f3349c13a62dc2
septem <qitengyuxi@gmail.com>
2025-03-20 09:06:29 +0900

    サイト全体にrevalidateを設定する

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,8 +4,6 @@ import Pagination from "../_componets/Pagination";
 import SearchField from "../_componets/SearchField";
 import { NEWS_LIST_LIMIT } from "@/app/_constants/index";
 
-export const revalidate = 0;
-
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
