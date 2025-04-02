2506d1e36f9c762310f932b6f6d4cb72094da5b6
septem <qitengyuxi@gmail.com>
2025-04-02 13:01:24 +0900

    10章まで完了

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "./_componets/Footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('http://localhost:3000'),
+  metadataBase: new URL('https://my-next-project2-xi.vercel.app'),
   title: {
     template: '%s | シンプルなコーポレートサイト',
     default: 'シンプルなコーポレートサイト',
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     images: ['/ogp.png'],
   },
   alternates: {
-    canonical: 'http://localhost:3000',
+    canonical: 'https://my-next-project2-xi.vercel.app',
   },
 };
 
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,22 +1,23 @@
 import { MetadataRoute } from 'next';
 import { getAllCategoryList, getAllNewsList } from './_libs/microcms';
 
-const buildUrl = (path?: string) => `http://localhost:3000${path ?? ''}`;
+const buildUrl = (path?: string) => `https://my-next-project2-xi.vercel.app${path ?? ''}`;
+// const buildUrl = (path?: string) => `http://localhost:3000${path ?? ''}`;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const newsContents = await getAllNewsList();
-  const categoryContents = await getAllCategoryList();
+  // const newsContents = await getAllNewsList();
+  // const categoryContents = await getAllCategoryList();
 
-  const newsUrls: MetadataRoute.Sitemap = newsContents.map((content) => ({
-    url: buildUrl(`/news/${content.id}`),
-    lastModified: content.revisedAt,
-  }));
-  const categoryUrls: MetadataRoute.Sitemap = categoryContents.map(
-    (content) => ({
-      url: buildUrl(`/news/category/${content.id}`),
-      lastModified: content.revisedAt,
-    })
-  );
+  // const newsUrls: MetadataRoute.Sitemap = newsContents.map((content) => ({
+  //   url: buildUrl(`/news/${content.id}`),
+  //   lastModified: content.revisedAt,
+  // }));
+  // const categoryUrls: MetadataRoute.Sitemap = categoryContents.map(
+  //   (content) => ({
+  //     url: buildUrl(`/news/category/${content.id}`),
+  //     lastModified: content.revisedAt,
+  //   })
+  // );
 
   const now = new Date();
 
@@ -37,7 +38,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: buildUrl('/news'),
       lastModified: now,
     },
-    ...newsUrls,
-    ...categoryUrls,
+    // ...newsUrls,
+    // ...categoryUrls,
   ];
 }
